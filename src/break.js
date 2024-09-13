dd21cea5ca3e5f78bf65fe9d0e688bfa4c332598
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2024-09-13 14:22:50 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,17 @@
 const numbers = [];
+let operationsAvoided = 0;
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 50) {
+        operationsAvoided += (numbers.length - i - 1);
+        console.log("Nombre superieur à 50")
+        break;
+    }
     console.log(numbers[i]);
 }
+
+console.log("Nombre d'opération évitées :" + operationsAvoided);
