bb1a74ee79242039b393771aa0169df3e35045ed
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2024-09-13 14:42:50 +0200

    feat: continue better not finished

--- src/continue.js
+++ src/continue.js
@@ -1,8 +1,13 @@
 const numbers = [];
+let result = 0;
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    result += numbers[i];
 }
+
+console.log(result);
